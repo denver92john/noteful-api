b5891371f49cfb8731f5a508aac2eeeac04455f2
Burl Adkins <adkins1393@gmail.com>
2019-10-12 01:18:56 -0400

    wrote routers to perfrom CRUD

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
+const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
@@ -14,6 +16,10 @@ const morganOptions = (NODE_ENV === 'production')
 app.use(morgan(morganOptions));
 app.use(helmet());
 app.use(cors());
+
+app.use('/api/folders', foldersRouter);
+
+app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
