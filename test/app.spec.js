0d00c6e32d77b9c17f747c31e75aeb1418d8c32c
Burl Adkins <adkins1393@gmail.com>
2019-10-16 14:48:44 -0400

    preparred to deploy server

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,12 +1,8 @@
-/*
-const expect = require('chai').expect;
-const request = require('supertest');
-*/
 const app = require('../src/app');
 
 describe('App', () => {
     it('GET / respons with 200 containing "Hello, world!"', () => {
-        return request(app)
+        return supertest(app)
             .get('/')
             .expect(200, 'Hello, world!')
     });
